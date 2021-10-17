bd2b2423b424a5fb6f076b603bbed8bf56b147c0
Santiago <santiagonicolas.cejas@gmail.com>
2021-10-16 23:32:28 -0300

    Home y navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () =>{
+    return(
+    <header className="header">
+        <nav className ="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Productos</li>
+                <li><Contacto></Contacto></li>
+            </ul>
+        </nav>
+   </header> 
+    );
+};
