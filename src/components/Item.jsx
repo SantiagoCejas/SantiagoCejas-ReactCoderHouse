45be1d154bd4df67988c24f64fc785fe3cea8d03
Santiago <santiagonicolas.cejas@gmail.com>
2021-10-25 04:48:04 -0300

    Santiago Cejas

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,12 +4,12 @@ const Item =({name, grape, price, image})=> {
     return (
         <div className="item">
             <div>
+                <img src={image} alt={name} />
+            </div>
+            <div className="itemDetalle">
                 <h3>{name}</h3>
                 <p>{grape}</p>
                 <p>${price}</p>
-            </div>
-            <div>
-                <img src={image} alt={name} />
             </div>
         </div>
     )
